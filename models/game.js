b9f51e5e661b984955b8e5127e853662f14b763d
Patrick <kelly.patj@gmail.com>
2023-02-23 13:35:39 -0500

    adds game model

---
+++ models/game.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Game extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Game.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    GA_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 0,
+        max: 5,
+      }
+    },
+    owner: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Profiles',
+        key: 'id',
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Game',
+  });
+  return Game;
+};
